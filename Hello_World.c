1c443ffea010905da4c7edf8b9b7299600afdb75
Ihor Bakulin <ihor.bakulin.sp@gmail.com>
2022-12-05 17:18:04 +0200

    first commit

---
+++ Hello_World.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+
+	printf("Group number - 2\n");
+	printf("Full name - Ihor Bakulin\n");
+	printf("Email - ihor.bakulin.sp@gmail.com\n");
+	printf("Repository link - https://github.com/Ihorbakulin/Pure_C_Training_01.git \n");
+	printf("Shared folder link - https://drive.google.com/drive/folders/1nvqObJfZNJv8KtX8Ht4jeHiaz-jK6l77?usp=share_link \n");
+	
+	return 0;
+}
